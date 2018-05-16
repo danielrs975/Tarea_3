df36a50c145bdc2241e0e4527484a2f3d360f5e6
German Robayo Paz <germanrobayo33@gmail.com>
2018-05-16 10:16:57 -0400

    anade esqueleto para Seguridad

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
+from Seguridad import Seguridad
+import unittest
 # Create your tests here.
+
+class TestSeguridad(unittest.TestCase):
+
+    def test_registrar_usuario(self):
+        seguridad = Seguridad()
